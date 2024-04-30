42a27d0e2a157ca9c098cff8ac1840300bdc9959
ingcarrillo12 <ing.carrillo12@gmail.com>
2024-04-29 21:59:16 -0500

    actualizando import pool

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -1,8 +1,8 @@
-import db from '../config/db.js';
+import {pool} from '../config/pool.js';
 
 export const getAllBookings = async (req, res) => {
     try {
-        const [bookings, fields] = await db.query('SELECT * FROM bookings');
+        const [bookings, fields] = await pool.query('SELECT * FROM bookings');
         res.json(bookings);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -11,7 +11,7 @@ export const getAllBookings = async (req, res) => {
 
 export const getBookingById = async (req, res) => {
     try {
-        const [booking, fields] = await db.query('SELECT * FROM bookings WHERE idbookings = ?', [req.params.id]);
+        const [booking, fields] = await pool.query('SELECT * FROM bookings WHERE idbookings = ?', [req.params.id]);
         if (booking.length === 0) {
             return res.status(404).json({ message: 'Booking not found' });
         }
@@ -24,7 +24,7 @@ export const getBookingById = async (req, res) => {
 export const createBooking = async (req, res) => {
     try {
         const { codigo_habitacion, nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida } = req.body;
-        const [result] = await db.query('INSERT INTO bookings (codigo_habitacion, nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida) VALUES (?, ?, ?, ?, ?, ?)', [codigo_habitacion, nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida]);
+        const [result] = await pool.query('INSERT INTO bookings (codigo_habitacion, nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida) VALUES (?, ?, ?, ?, ?, ?)', [codigo_habitacion, nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida]);
         res.status(201).json({ message: 'Booking created', bookingId: result.insertId });
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -34,7 +34,7 @@ export const createBooking = async (req, res) => {
 export const updateBooking = async (req, res) => {
     try {
         const { nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida } = req.body;
-        const [result] = await db.query('UPDATE bookings SET nombre_cliente = ?, telefono_cliente = ?, fecha_reservación = ?, fecha_entrada = ?, fecha_salida = ? WHERE idbookings = ?', [nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida, req.params.id]);
+        const [result] = await pool.query('UPDATE bookings SET nombre_cliente = ?, telefono_cliente = ?, fecha_reservación = ?, fecha_entrada = ?, fecha_salida = ? WHERE idbookings = ?', [nombre_cliente, telefono_cliente, fecha_reservación, fecha_entrada, fecha_salida, req.params.id]);
         if (result.affectedRows === 0) {
             return res.status(404).json({ message: 'Booking not found' });
         }
@@ -46,7 +46,7 @@ export const updateBooking = async (req, res) => {
 
 export const deleteBooking = async (req, res) => {
     try {
-        const [result] = await db.query('DELETE FROM bookings WHERE idbookings = ?', [req.params.id]);
+        const [result] = await pool.query('DELETE FROM bookings WHERE idbookings = ?', [req.params.id]);
         if (result.affectedRows === 0) {
             return res.status(404).json({ message: 'Booking not found' });
         }
@@ -60,7 +60,7 @@ export const deleteBooking = async (req, res) => {
 export const bookingCompleteById = async (req, res) => {
     try {
         // Esta es una consulta de ejemplo, debes reemplazarla con la consulta real que necesitas.
-        const [booking, fields] = await db.query(`
+        const [booking, fields] = await pool.query(`
             SELECT *
             FROM bookings
             JOIN rooms  ON rooms.codigo_habitacion = bookings.idbookings
